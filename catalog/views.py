522811d9d12bd6031530c995e0c71617ef940e69
Elijah Piper <ep297659@dexcom.com>
2020-09-24 04:42:32 -0700

    Began implementing Book list view.

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
 	"""View function for home page of the site."""
@@ -28,3 +29,6 @@ def index(request):
 
 	# Render the HTML template index.html with the data in the context variable
 	return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+	model = Book
