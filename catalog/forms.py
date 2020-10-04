f21cb4d045509ca2a84004bc4707f0bc308f989d
Elijah Piper <ep297659@dexcom.com>
2020-10-04 04:47:55 -0700

    Large testing commit.

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,29 @@
+import datetime
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 class RenewBookForm(forms.Form):
-	renewal_date = forms.DateField(help_text="Enter a data between now and 4 weeks (default 3).")
+	renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
+	def clean_renewal_date(self):
+		"""
+		Validates the renewal date by specified parameters.
+		Acts sort of like a filter. It does nothing if it catches nothing wrong
+			with the data and just returns the what was passed to it; if there 
+			is something wrong caught, then a ValidationError is returned with
+			a message to be displayed.
+		"""
+		data = self.cleaned_data['renewal_date']
+
+		# Check if a date is not in the past.
+		if data < datetime.date.today():
+			raise ValidationError(_('Invalid date - renewal in past'))
+
+		# Check if a date is in the allowed range (+4 weeks from today).
+		if data > datetime.date.today() + datetime.timedelta(weeks=4):
+			raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+		# Remember to always return the cleaned data.
+		return data
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from catalog.models import Book, Author, BookInstance, Genre
+import datetime
+
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.forms import RenewBookForm
 
 def index(request):
 	"""View function for home page of the site."""
@@ -36,10 +44,42 @@ def index(request):
 	# Render the HTML template index.html with the data in the context variable
 	return render(request, 'index.html', context=context)
 
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+	"""View function for the renew book forms for librarians."""
+	book_instance = get_object_or_404(BookInstance, pk=pk)
+
+	# If this is a POST request then process the Form data
+	if request.method == 'POST':
+
+		# Create a form instance and populate it with data from the request (binding)
+		form = RenewBookForm(request.POST)
+
+		# Check if the form is valid:
+		if form.is_valid():
+			# Process  the data in form.cleaned_data as required (here we just write it to the model due_back field).
+			book_instance.due_back = form.cleaned_data['renewal_date']
+			book_instance.save()
+
+			# Redirect to a new URL
+			return HttpResponseRedirect(reverse('all-borrowed'))
+
+	# If this a a GET (or any other method) create the default form.
+	else:
+		proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+		form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+	context = {
+		'form': form,
+		'book_instance': book_instance,
+	}
+
+	return render(request, 'catalog/book_renew_librarian.html', context)
+
 
 class BookListView(generic.ListView):
 	model = Book
-	paginate_by = 50
+	paginate_by = 10
 
 class BookDetailView(generic.DetailView):
 	model = Book
@@ -47,7 +87,7 @@ class BookDetailView(generic.DetailView):
 
 class AuthorListView(generic.ListView):
 	model = Author
-	paginate_by = 50
+	paginate_by = 10
 
 class AuthorDetailView(generic.DetailView):
 	model = Author
@@ -71,3 +111,45 @@ class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
 
 	def get_queryset(self):
 		return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Author
+	fields = '__all__'
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Author
+	fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Author
+	success_url = reverse_lazy('authors')
+
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Book
+	fields = '__all__'
+
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Book
+	fields = '__all__'
+
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+	permission_required = 'catalog.can_mark_returned'
+
+	model = Book
+	fields = '__all__'
